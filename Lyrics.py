c0027ac10777ee25a9d453ae48f7b19898367270
Garabella15 <owhon.raymond619@gmail.com>
2022-06-28 09:02:44 +0100

    updating the lyrics

--- Lyrics.py
+++ Lyrics.py
@@ -1,35 +1,39 @@
+from pprint import pp
 import requests
-from numpy import average
 import json
+from numpy import average
 
-from app import fetch_lyrics_of_songs, artist_name
 
-total_words =[]
-artist = artist_name
-def fetch_lyrics():
+from app import fetch_lyrics_of_songs, artist_name              
+
+# total_words =[]
+total_lyrics = {}
+def get_lyrics():
     artist = artist_name
     lists_of_title = fetch_lyrics_of_songs()
     for title in lists_of_title:
-
         try:
             url = "https://api.lyrics.ovh/v1/" + artist + "/" + title
         
             response = requests.get(url).json()
+            lyrics = response.get("lyrics")
+            total_lyrics.update({title: lyrics})
         except json.decoder.JSONDecodeError:
             print('')   
             if response == None or response == '':
                 print("No lyrics found")
                 pass
-            else:
-    
-                lyrics = response.get("lyrics")
-                total_words.append(lyrics)
         
-        
+get_lyrics()
+count = 0
+number = 1
+for k,v in total_lyrics.items():
+    if v == None or v == '':
+        pass
+    else:
+        words = ''.join(v)   
+        words = len(v.split())
+        count += words        
+    number += 1
 
-fetch_lyrics()
-total_words = " ".join(total_words)
-print(average(len(total_words.split()))) 
-
-fetch_lyrics()
-# count_average_words_in_song()
+    print(count)
